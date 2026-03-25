1465f054b81057991ad7a7ae9fb936b613d224f5
Azamat2811 <hexogonn@mail.ru>
2026-03-25 12:22:03 +0500

    Update usersService.go

--- services/usersService.go
+++ services/usersService.go
@@ -86,11 +86,8 @@ func UpdateUser(id int, data map[string]interface{}) (models.Users, error) {
 	_, err = db.DB.Exec(
 
 		"UPDATE users SET username = $1, phone = $2, email = $3, age = $4, city = $5 WHERE id = $6",
-		user.Username,
-		user.Phone,
-		user.Email,
+		user.Name,
 		user.Age,
-		user.City,
 		id,
 	)
 	if err != nil {
@@ -101,12 +98,9 @@ func UpdateUser(id int, data map[string]interface{}) (models.Users, error) {
 
 func FullUpdateUsers(id int, updated models.Users) (models.Users, error) {
 	result, err := db.DB.Exec(
-		"UPDATE users SET username = $1, phone = $2, email = $3, age = $4, city = $5 WHERE id = $6",
-		updated.Username,
-		updated.Phone,
-		updated.Email,
+		"UPDATE users SET name = $1, age = $2, WHERE id = $3",
+		updated.Name,
 		updated.Age,
-		updated.City,
 		id,
 	)
 
@@ -124,4 +118,84 @@ func FullUpdateUsers(id int, updated models.Users) (models.Users, error) {
 
 	updated.ID = id
 	return updated, nil
+}
+
+func GetUsersPagination(page int, limit int) ([]models.Users, error) {
+   
+    offset := (page - 1) * limit
+
+    rows, err := db.DB.Query(
+        "SELECT id, name, age FROM users ORDER BY id LIMIT $1 OFFSET $2",
+        limit,
+        offset,
+    )
+    if err != nil {
+        return nil, err
+    }
+    defer rows.Close()
+
+    var users []models.Users
+    for rows.Next() {
+        var user models.Users
+        if err := rows.Scan(&user.ID, &user.Name, &user.Age); err != nil {
+            return nil, err
+        }
+        users = append(users, user)
+    }
+
+    if len(users) == 0 {
+        return []models.Users{}, nil
+    }
+
+    return users, nil
+}
+
+func GetUsersByName(nameFilter string) ([]models.Users, error) {
+   
+    query := "SELECT id, name, age FROM users WHERE name ILIKE $1 ORDER BY id"
+    
+    searchPattern := "%" + nameFilter + "%"
+
+    rows, err := db.DB.Query(query, searchPattern)
+    if err != nil {
+        return nil, err
+    }
+    defer rows.Close()
+
+    var users []models.Users
+    for rows.Next() {
+        var user models.Users
+       
+        if err := rows.Scan(&user.ID, &user.Name, &user.Age); err != nil {
+            return nil, err
+        }
+        users = append(users, user)
+    }
+
+    if users == nil {
+        return []models.Users{}, nil
+    }
+
+    return users, nil
+}
+
+func GetUserStats() (map[string]interface{}, error) {
+    var avg float64
+    var min, max, count int
+
+    query := "SELECT AVG(age), MIN(age), MAX(age), COUNT(*) FROM users"
+    
+    err := db.DB.QueryRow(query).Scan(&avg, &min, &max, &count)
+    if err != nil {
+        return nil, err
+    }
+
+    stats := map[string]interface{}{
+        "average_age": avg,
+        "min_age":     min,
+        "max_age":     max,
+        "total_users": count,
+    }
+
+    return stats, nil
 }
